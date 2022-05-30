5f43420a294c3717a796f71c84901b77760f1b26
onehousesilver <onehousesilver@naver.com>
2022-05-31 00:00:10 +0900

    attrs를 이용한 속성값 변경

--- src/App.js
+++ src/App.js
@@ -4,18 +4,19 @@ const Parents = styled.div`
   display: flex;
 `;
 
-const Btn = styled.button`
-  color: white;
+const Input = styled.input.attrs({ required: true })`
   background-color: tomato;
-  border: 0;
-  border-radius: 15px;
 `;
 
 function App() {
   return (
     <Parents>
-      <Btn> Log in </Btn>
-      <Btn as="a" href="/"> Log in </Btn>
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Parents>
   );
 }
